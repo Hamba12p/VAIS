95d7dd46e539e9ed079b26d10b4d1376cbc3e2d1
Hamba12p <ismaelhamba@gmail.com>
2026-06-22 10:17:42 +0300

    Use local work page images

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,19 +12,19 @@ export function slugToName(slug: string): string {
     .join(' ');
 }
 
-const PIN_IMAGES = [
-  'https://i.pinimg.com/736x/1b/40/ea/1b40ea2f88b59e04a4580fce9bdb1e26.jpg',
-  'https://i.pinimg.com/736x/80/fb/0a/80fb0a530e76c637cab64115f4b0e025.jpg',
-  'https://i.pinimg.com/736x/29/df/1b/29df1bac4893a448fff00094dbcd6d7f.jpg',
-  'https://i.pinimg.com/736x/7f/a6/bc/7fa6bc26abb2319845ebb73d7eb0f079.jpg',
-  'https://i.pinimg.com/736x/e7/f6/9a/e7f69a8643c253b1b7587c162948f392.jpg',
-  'https://i.pinimg.com/736x/02/df/ce/02dfce8e7e33dcb5c7b9c82e95fa980c.jpg',
-  'https://i.pinimg.com/736x/72/6a/d4/726ad41b42327ea20f020502267b8094.jpg',
+const PROJECT_IMAGES = [
+  '/images/1b40ea2f88b59e04a4580fce9bdb1e26.jpg',
+  '/images/80fb0a530e76c637cab64115f4b0e025.jpg',
+  '/images/29df1bac4893a448fff00094dbcd6d7f.jpg',
+  '/images/7fa6bc26abb2319845ebb73d7eb0f079.jpg',
+  '/images/e7f69a8643c253b1b7587c162948f392.jpg',
+  '/images/02dfce8e7e33dcb5c7b9c82e95fa980c.jpg',
+  '/images/726ad41b42327ea20f020502267b8094.jpg',
 ];
 
 export function getPatternedImage(index: number): string {
-  const n = PIN_IMAGES.length;
+  const n = PROJECT_IMAGES.length;
   const frac = (index * 0.618033988749895) % 1; // golden-ratio spacing
   const pos = Math.floor(frac * n);
-  return PIN_IMAGES[pos];
+  return PROJECT_IMAGES[pos];
 }
